4c51204286922d566bc3b6fb134843e2f2455e31
Ronald <Ronald1985@tutamail.com>
2021-01-02 19:29:40 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module stayawake
+
+go 1.15
+
+require (
+	github.com/getlantern/systray v1.1.0
+	golang.org/x/sys v0.0.0-20201231184435-2d18734c6014 // indirect
+)
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+/*
+#include <windows.h>
+#include <stdio.h>
+void pressKeys(void) {
+    INPUT inp;
+
+    inp.type = INPUT_KEYBOARD;
+    inp.ki.wScan = 0;
+    inp.ki.time = 0;
+    inp.ki.dwExtraInfo = 0;
+
+    inp.ki.wVk = 0x7D;
+    inp.ki.dwFlags = 0;
+    SendInput(1, &inp, sizeof(INPUT));
+
+    inp.ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, &inp, sizeof(INPUT));
+}
+*/
+import "C"
+
+import (
+	"fmt"
+	"stayawake/icon"
+	"syscall"
+	"time"
+	"unsafe"
+
+	"github.com/getlantern/systray"
+)
+
+var (
+	kernel32        = syscall.NewLazyDLL("kernel32.dll")
+	procCreateMutex = kernel32.NewProc("CreateMutexW")
+)
+
+func CreateMutex(name string) (uintptr, error) {
+	ret, _, err := procCreateMutex.Call(
+		0,
+		0,
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(name))),
+	)
+	switch int(err.(syscall.Errno)) {
+	case 0:
+		return ret, nil
+	default:
+		return ret, err
+	}
+}
+
+func main() {
+	_, err := CreateMutex("StayAwake")
+	if err != nil {
+		fmt.Println("Application already running, quitting.")
+		return
+	}
+	onExit := func() {
+		return
+	}
+	systray.Run(onReady, onExit)
+}
+
+func onReady() {
+	var enabled bool = true
+	var seconds int = 20
+	systray.SetIcon(icon.Data)
+	systray.SetTitle("Stay Awake")
+	systray.SetTooltip("Stay Awake")
+	mQuitOrig := systray.AddMenuItem("Quit", "Quit the whole app")
+	go func() {
+		<-mQuitOrig.ClickedCh
+		fmt.Println("Requesting quit")
+		systray.Quit()
+		fmt.Println("Finished quitting")
+	}()
+
+	go func() {
+		systray.AddSeparator()
+		mChecked := systray.AddMenuItemCheckbox("Enabled", "Check Me", true)
+
+		for {
+			select {
+			case <-mChecked.ClickedCh:
+				if mChecked.Checked() {
+					mChecked.Uncheck()
+					enabled = false
+				} else {
+					mChecked.Check()
+					enabled = true
+				}
+			}
+		}
+
+	}()
+
+	go func() {
+		for {
+			if enabled == true {
+				C.pressKeys()
+			}
+			time.Sleep(time.Duration(seconds) * time.Second)
+		}
+	}()
+}
